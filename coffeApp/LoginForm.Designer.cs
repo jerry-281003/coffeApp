cced05e0f8213fef000ac950b261c9920e1e7749
Jerry-281003 <thanhjerry03@gmail.com>
2023-10-11 10:17:40 +0700

    login view

---
+++ coffeApp/LoginForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace coffeApp
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelUserName = new Label();
+            labelPassword = new Label();
+            txtUserNameLogin = new TextBox();
+            txtPasswordLogin = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // labelUserName
+            // 
+            labelUserName.AutoSize = true;
+            labelUserName.Location = new Point(124, 55);
+            labelUserName.Margin = new Padding(2, 0, 2, 0);
+            labelUserName.Name = "labelUserName";
+            labelUserName.Size = new Size(68, 15);
+            labelUserName.TabIndex = 0;
+            labelUserName.Text = "User Name:";
+            // 
+            // labelPassword
+            // 
+            labelPassword.AutoSize = true;
+            labelPassword.Location = new Point(125, 89);
+            labelPassword.Margin = new Padding(2, 0, 2, 0);
+            labelPassword.Name = "labelPassword";
+            labelPassword.Size = new Size(57, 15);
+            labelPassword.TabIndex = 1;
+            labelPassword.Text = "Password";
+            // 
+            // txtUserNameLogin
+            // 
+            txtUserNameLogin.Location = new Point(200, 55);
+            txtUserNameLogin.Margin = new Padding(2, 2, 2, 2);
+            txtUserNameLogin.Name = "txtUserNameLogin";
+            txtUserNameLogin.Size = new Size(106, 23);
+            txtUserNameLogin.TabIndex = 2;
+            // 
+            // txtPasswordLogin
+            // 
+            txtPasswordLogin.Location = new Point(200, 89);
+            txtPasswordLogin.Margin = new Padding(2, 2, 2, 2);
+            txtPasswordLogin.Name = "txtPasswordLogin";
+            txtPasswordLogin.Size = new Size(106, 23);
+            txtPasswordLogin.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(220, 142);
+            btnLogin.Margin = new Padding(2, 2, 2, 2);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(78, 20);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(560, 270);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPasswordLogin);
+            Controls.Add(txtUserNameLogin);
+            Controls.Add(labelPassword);
+            Controls.Add(labelUserName);
+            Margin = new Padding(2, 2, 2, 2);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelUserName;
+        private Label labelPassword;
+        private TextBox txtUserNameLogin;
+        private TextBox txtPasswordLogin;
+        private Button btnLogin;
+    }
+}
---
+++ coffeApp/LoginForm.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.SqlClient;
+
+
+namespace coffeApp
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+        string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CafeManagemnt;User ID=sa;Password=123;";
+
+
+
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUserNameLogin.Text;
+            string password = txtPasswordLogin.Text;
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Check the user's credentials
+                string query = "SELECT COUNT(*) FROM [User] WHERE UserName = @Username AND Password = @Password";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", username);
+                    command.Parameters.AddWithValue("@Password", password); // Note: Password should be hashed in a real application.
+
+                    int userCount = (int)command.ExecuteScalar();
+
+                    if (userCount == 1)
+                    {
+                        MessageBox.Show("Login successful");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid username or password");
+                    }
+                }
+            }
+        }
+    }
+}
